358fc40d2c0dff412908ba7694d2bd41ffd172cb
Nolan Berryhill <nberryhill@my365.bellevue.edu>
2023-10-22 15:58:46 -0500

    merge

---
+++ week-1/ts-examples/person.class.ts
@@ -0,0 +1,15 @@
+import { IPerson } from "./person.interface";
+
+class Person implements IPerson {
+
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+let myName = new Person("Nolan", "Berryhill");
+console.log(`My name is ${myName.firstName} ${myName.lastName}`)
