ab5dc807fe4c4d41d2c80a8a6552e92bf03794ea
Nolan Berryhill <nberryhill@my365.bellevue.edu>
2023-10-22 15:10:15 -0500

    merge

--- week-1/hello-world/src/app/app.component.ts
+++ week-1/hello-world/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+  myWorld = "You are now in Nolan Berryhill's world!"
 }
