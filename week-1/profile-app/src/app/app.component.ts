602e5dc10cfd976b4276c6d72a25a58e0910f0a2
Nolan Berryhill <nberryhill@my365.bellevue.edu>
2023-10-22 17:22:39 -0500

    merge

--- week-1/profile-app/src/app/app.component.ts
+++ week-1/profile-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'profile-app';
+  assignment: string = 'Assignment 1.5 - Components';
 }
