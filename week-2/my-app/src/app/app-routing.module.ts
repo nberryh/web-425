4d60d59080af4b89c12f6bcfe1cf476a1c527352
Nolan Berryhill <nberryhill@my365.bellevue.edu>
2023-10-28 16:28:58 -0500

    merge

--- week-2/my-app/src/app/app-routing.module.ts
+++ week-2/my-app/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
+import { Component } from '@angular/core';
+// Name: Nolan Berryhill
+// File: app-routing.module.ts
+// Description: Routing module for the my-app
+// Date: 10/29/2023
+
+// Imports NgModules and Routes
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Import components
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+// Define routes
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
