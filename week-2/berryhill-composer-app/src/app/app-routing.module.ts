5bba9ab758b4c3ef674555441e25fb194564cfe2
Nolan Berryhill <nberryhill@my365.bellevue.edu>
2023-10-28 18:20:13 -0500

    merge

--- week-2/berryhill-composer-app/src/app/app-routing.module.ts
+++ week-2/berryhill-composer-app/src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
+// Name: Nolan Berryhill
+// File: app-routing.module.ts
+// Description: Routing module for the app
+// Date: 10/29/2023
+
+// Imports Routes and angular
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Import components
+import { AboutComponent } from './about/about.component';
+import { ComposerListComponent } from './composer-list/composer-list.component';
+import { ContactComponent } from './contact/contact.component';
+
+// Define routes
+const routes: Routes = [
+  {
+    path: '',
+    component: ComposerListComponent
+  },
+  {
+    path: 'composer-list',
+    component: ComposerListComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- week-2/berryhill-composer-app/src/app/composer-list/composer-list.component.ts
+++ week-2/berryhill-composer-app/src/app/composer-list/composer-list.component.ts
@@ -1,15 +1,42 @@
+// Name: Nolan Berryhill
+// File: composer-list.module.ts
+// Description: TypeScript for the composer list
+// Date: 10/29/2023
+
+// import
 import { Component, OnInit } from '@angular/core';
+
+// export Composer
+export class Composer {
+  fullName: string;
+  genre: string;
+
+  constructor(fullName: string, genre: string) {
+    this.fullName = fullName;
+    this.genre = genre;
+  }
+}
 
 @Component({
   selector: 'app-composer-list',
   templateUrl: './composer-list.component.html',
   styleUrls: ['./composer-list.component.css']
 })
-export class ComposerListComponent implements OnInit {
 
-  constructor() { }
+// Export ComposerListComponent
+export class ComposerListComponent {
+  // Create array
+  composers: Array<Composer>;
 
-  ngOnInit(): void {
+  constructor() {
+    this.composers = [
+      new Composer('Wolfgang Amadeus Mozart', 'Classical'),
+      new Composer('John Adams', 'Classical'),
+      new Composer('Laurie Anderson', "Modern"),
+      new Composer('Trisha Brown', 'Choregrapher'),
+      new Composer('dietrich Buxtehude', 'Classical')
+    ];
   }
+
 
 }
