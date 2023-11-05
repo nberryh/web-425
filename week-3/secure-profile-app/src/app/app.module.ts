88327e781a0d726e0670a443b933a2d4480b47ad
Nolan Berryhill <nberryhill@my365.bellevue.edu>
2023-11-05 15:57:04 -0600

    merge

--- week-3/secure-profile-app/src/app/app.module.ts
+++ week-3/secure-profile-app/src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyDetailsComponent } from './my-details/my-details.component';
 import { MyImageComponent } from './my-image/my-image.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutes } from './app.routing';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyDetailsComponent,
-    MyImageComponent
+    MyImageComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
