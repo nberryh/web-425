40f5e9b96b0e6c6e459cdf1c64281729dca4c3d5
Nolan Berryhill <nberryhill@my365.bellevue.edu>
2023-11-18 17:21:31 -0600

    merge

--- week-5/in-n-out-booksp1/src/app/app.component.ts
+++ week-5/in-n-out-booksp1/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'in-n-out-booksp1';
