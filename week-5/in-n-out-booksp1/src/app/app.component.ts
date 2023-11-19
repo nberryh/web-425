f17bd03b5d242fd3087dc716d52b438677db7db8
Nolan Berryhill <nberryhill@my365.bellevue.edu>
2023-11-18 22:09:04 -0600

    merge

--- week-5/in-n-out-booksp1/src/app/app.component.ts
+++ week-5/in-n-out-booksp1/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'in-n-out-booksp1';
+
+  assignment: string;
+
+  constructor() {
+    this.assignment = 'Exercise 5.2 - Navigation and Layout'
+  }
 }
