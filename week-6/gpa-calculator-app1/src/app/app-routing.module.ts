4fd24f3d0ccc8dbcc2449a9c56ad613848e306d0
Nolan Berryhill <nberryhill@my365.bellevue.edu>
2023-12-03 16:29:44 -0600

    merge

--- week-6/gpa-calculator-app1/src/app/app-routing.module.ts
+++ week-6/gpa-calculator-app1/src/app/app-routing.module.ts
@@ -1,12 +1,49 @@
+// Name: Nolan Berryhill
+// File: app-routing.module.ts
+// Date: 12/03/2023
+// Description: TS file for app-routing
+
+// Angular specific imports
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseLayoutComponent } from './base-layout/base-layout.component';
+import { HomeComponent } from './home/home.component';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
+// Routes for routing paths
 const routes: Routes = [
-  // Define your routes here
+  {
+    path: '',
+    component: BaseLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ]
+  },
+  {
+    path: 'session',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'not-found',
+        component: NotFoundComponent
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'session/not-found'
+  }
 ];
 
+// Imports and exports for NgModule
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
+
+// Export the AppRoutingModule
 export class AppRoutingModule {}
